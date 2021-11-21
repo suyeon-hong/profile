2ef30b026c5346dab36a0dfcc2185ae6f418d059
bellahong95 <bellahong95@gmail.com>
2021-11-21 14:43:04 +0900

    swiper 확장기능

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-new Swiper("#wrap");
+new Swiper("#wrap", {
+    loop: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 50,
+
+    navigation:{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+
+    pagination:{
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true,
+    },
+
+    effect: 'coverflow',
+    coverflowEffect:{
+        rotate: 30,
+        strech: -100,
+        depth: 400,
+        slideShadows: false,
+    }
+});
